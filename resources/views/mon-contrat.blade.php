09afe6cf17803fb12d615ab904f01cef42b2be6c
yacinend <ndiayeyacine2004@hotmail.com>
2020-06-03 14:53:12 +0200

    modif vue contrat

--- resources/views/mon-contrat.blade.php
+++ resources/views/mon-contrat.blade.php
@@ -9,10 +9,89 @@ $notification = (isset($_SESSION["numberOfBillsNonPaid"])) ? $_SESSION["numberOf
 <div class="container">
     <div class="row" style="margin-top:10%">
         <div class="col-md-8 col-md-offset-2">
-          <div class="panel-heading" style="margin-top:10px;margin-bottom:20px;line-height: 2.4;">
+          <div class="panel-heading contract-panel-heading">
             <h3>Le contrat est au nom de </h3>
-           <strong style="margin-top:10px">{{ Auth::user()->first_name.' '.Auth::user()->name}}</strong></div>
-            <div class="panel panel-default">
+           <strong style="margin-top:10px;text-transform: capitalize;">{{ Auth::user()->first_name.' '.Auth::user()->name}}</strong>
+
+           <div class="row" style="margin-left: 1%;">
+            <div class="col-xl-3 col-md-6">
+              <div class="card contract-header-card">
+                <!-- Card body -->
+                <div class="card-body-contract">
+                  <div class="row" style="margin-left: 1%;">
+                    <div class="col-md-8">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Type d'offre</h5>
+                      <span class="h2 font-weight-bold mb-0">@php $wording_offer = (Auth::user()->user_type == 2) ? "Woyofal" : "Classique"; echo $wording_offer; @endphp</span>
+                    </div>
+                    <div class="col-auto">
+                      <div>
+                        <i class="fas fa-id-badge fa-3x contract-header-icon" style="color: rgba(77,138,185,1);"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+              <div class="card contract-header-card">
+                <!-- Card body -->
+                <div class="card-body-contract">
+                  <div class="row" style="margin-left: 1%;">
+                    <div class="col-md-8">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Statut du contrat</h5>
+                      <span class="h2 font-weight-bold mb-0">Actif</span>
+                    </div>
+                    <div class="col-auto">
+                      <div>
+                        <i class="fas fa-toggle-on fa-3x" style="color: forestgreen;"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+              <div class="card contract-header-card">
+                <!-- Card body -->
+                <div class="card-body-contract">
+                  <div class="row" style="margin-left: 1%;">
+                    <div class="col-md-8">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Date de début du contrat</h5>
+                      <span class="h2 font-weight-bold mb-0">21/12/2018</span>
+                    </div>
+                    <div class="col-auto">
+                      <div>
+                        <i class="fas fa-calendar-alt fa-3x" style="color: rgba(77,138,185,1);"></i>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+              <div class="card contract-header-card">
+                <!-- Card body -->
+                <div class="card-body-contract">
+                  <div class="row" style="margin-left: 1%;">
+                    <div class="col-md-8">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Référence contrat</h5>
+                      <span class="h2 font-weight-bold mb-0">{{ Auth::user()->customerId }}</span>
+                    </div>
+                    <div class="col-auto">
+                      <div>
+                        <i class="fas fa-file-invoice fa-3x " style="color: rgba(77,138,185,1);"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+         </div>
+            <div class="panel panel-default" id= "contract-panel">
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -35,7 +114,7 @@ $notification = (isset($_SESSION["numberOfBillsNonPaid"])) ? $_SESSION["numberOf
 
                   <div class="col-md-6" style="margin-bottom:20px;">
                     <p>OFFRE</p>
-                    <span><strong>@php $wording_offer = (Auth::user()->user_type == 2) ? "Woyofal" : "Classique"; echo $wording_offer; @endphp</strong></span>
+                    <span class="recapData"><strong>@php $wording_offer = (Auth::user()->user_type == 2) ? "Woyofal" : "Classique"; echo $wording_offer; @endphp</strong></span>
                   </div>
 
                   <div class="col-md-6" style="margin-bottom:20px;">
@@ -45,12 +124,12 @@ $notification = (isset($_SESSION["numberOfBillsNonPaid"])) ? $_SESSION["numberOf
 
                   <div class="col-md-6" style="margin-bottom:20px;">
                     <p>STATUT</p>
-                    <span><strong> ACTIF </strong></span>
+                    <span class="recapData"><strong> ACTIF </strong></span>
                   </div>
 
                   <div class="col-md-6" style="margin-bottom:20px;">
                     <p>DATE DE D&Eacute;BUT DU CONTRAT</p>
-                    <span><strong> {{Auth::user()->created_at}} </strong></span>
+                    <span class="recapData"><strong> {{Auth::user()->created_at}} </strong></span>
                   </div>
 
                   <div class="col-md-12" style="margin-bottom:40px;" >
@@ -60,31 +139,31 @@ $notification = (isset($_SESSION["numberOfBillsNonPaid"])) ? $_SESSION["numberOf
 
                   <div class="col-md-6 col-md-offset-0" style="margin-bottom:20px;">
                     <p>POINT DE LIVRAISON</p>
-                    <span><strong>{{Auth::user()->address}}</strong></span>
+                    <span  class="recapData"><strong>{{Auth::user()->address}}</strong></span>
                   </div>
 
                   <div class="col-md-6 col-md-offset-0" style="margin-bottom:20px;">
                     <p>R&Eacute;F&Eacute;RENCE DU CONTRAT</p>
-                    <span><strong>{{ Auth::user()->customerId }}</strong></span>
+                    <span class="recapData"><strong>{{ Auth::user()->customerId }}</strong></span>
                   </div>
 
                   <div class="col-md-6 col-md-offset-0" style="margin-bottom:20px;">
                     <p>ADRESSE DE LA RESIDENCE</p>
-                    <span><strong>{{ Auth::user()->address }}</strong></span>
+                    <span class="recapData"><strong>{{ Auth::user()->address }}</strong></span>
                   </div>
 
                   <div class="col-md-6 col-md-offset-0" style="margin-bottom:20px;">
                     <p>ADRESSE DE FACTURATION</p>
-                    <span><strong>{{ Auth::user()->address }}</strong></span>
+                    <span class="recapData"><strong>{{ Auth::user()->address }}</strong></span>
                   </div>
 
                   <div class="col-md-6 col-md-offset-0" style="margin-bottom:20px;">
                     <p>OPTION ET PUISSANCE SOUSCRITE</p>
-                    <span><strong>9 KVA - Base</strong></span>
+                    <span class="recapData"><strong>9 KVA - Base</strong></span>
                   </div>
                   <div class="col-md-6 col-md-offset-0" style="margin-bottom:20px;">
                     <p>TYPE DE FACTURATION</p>
-                    <span><strong> BIMESTRE </strong></span>
+                    <span class="recapData"><strong> BIMESTRE </strong></span>
                   </div>
 
                   <div class="col-md-12" style="margin-bottom:40px;" >
@@ -96,7 +175,7 @@ $notification = (isset($_SESSION["numberOfBillsNonPaid"])) ? $_SESSION["numberOf
                       Si vous souhaitez parrainer un(e) proche, vous avez la possibilité
                        de lui partager votre lien personnalisé depuis lequel il ou elle
                        pourra souscrire son contrat <a href="127.0.0.1/parrainage/{{ Auth::user()->customerId }}">ici </a>.
-                        Le gain est de 20€ dans la cagnotte du parrain et 20€ dans celle du filleul, dès
+                        Le gain est de 10000FCFA dans la cagnotte du parrain et 10000FCFA dans celle du filleul, dès
                         lors que le contrat du filleul aura été activé.
                     </span>
                   </div>
