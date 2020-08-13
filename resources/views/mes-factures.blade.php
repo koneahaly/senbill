4d460ad172f136fe3bc4a76f16a134eb6da99fec
yacinend <ndiayeyacine2004@hotmail.com>
2020-08-13 19:28:19 +0200

    rollback à la version par

--- resources/views/mes-factures.blade.php
+++ resources/views/mes-factures.blade.php
@@ -93,8 +93,9 @@ $mapping_type_services = ['eau' => 'type_service_1', 'electricite' => 'type_serv
                         @endif
                         @if($value->status != "paid")
                         <!--  <td><button data-toggle="modal" data-target="#pay_bill" class="btn btn-danger btn-xs"> Régler</button> <br /> <span style="font-size:0.7em;font-weight: lighter;color:black;"> avant le {{$value->created_at}} <span></td> -->
-                        <!--     <td><button id="regler" onclick= class="btn btn-danger btn-xs"> Régler</button> <br /> <span style="font-size:0.7em;font-weight: lighter;color:black;"> avant le {{$value->created_at}} <span></td> -->
-                        <td><button class="pay" id="regler1" onclick="" data-ref="102" data-fullname="Alioune Faye" data-email="aliounefaye@gmail.com" data-phone="774563209">Régler PD</button><br /> <span style="font-size:0.7em;font-weight: lighter;color:black;"> avant le {{$value->created_at}} <span></td>
+                            <td><button id="regler" onclick= class="btn btn-danger btn-xs"> Régler</button> <br /> <span style="font-size:0.7em;font-weight: lighter;color:black;"> avant le {{$value->created_at}} <span></td>
+                        <!--  POUR REGLER VIA PAYDUNYA SANS REDIRECTION COMMENTER LA LIGNE DU DESSUS ET DECOMMENTER CELLE EN DESSOUS ET DECOMMENTER DANS LAYOUT.app LE CSS de PAYDUNYA, en bas le script paydunya
+                         <td><button class="pay" id="regler1" onclick="" data-ref="102" data-fullname="Alioune Faye" data-email="aliounefaye@gmail.com" data-phone="774563209">Régler PD</button><br /> <span style="font-size:0.7em;font-weight: lighter;color:black;"> avant le {{$value->created_at}} <span></td>-->
                           <td> n/a </td>
                           <input type='hidden' id="{{$value->month}}_status" value="{{$value->status}}" />
                         @endif
@@ -1261,7 +1262,7 @@ if($i == $limit){
 @section('scripts')
 <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.material.min.js"></script>
-<script src="https://paydunya.com/assets/psr/js/psr.paydunya.min.js"></script>
+<!-- <script src="https://paydunya.com/assets/psr/js/psr.paydunya.min.js"></script> -->
 <script>
 $(document).ready(function() {
     $('#regler').click(function(){
