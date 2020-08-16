12f2431edde72c3c75c448e144cc5286ab93a116
ahkone <koneahaly@gmail.com>
2020-08-16 14:46:41 +0200

    add route to verify mail

--- routes/web.php
+++ routes/web.php
@@ -143,3 +143,5 @@ Route::get('sendhtmlemail','MailController@html_email');
 Route::get('sendpaymentokemail','MailController@paymentOK_email');
 Route::get('sendnewbillemail','MailController@newBill_email');
 Route::get('sendlatebillemail','MailController@lateBill_email');
+
+Route::get('verify-email/{name?}','Auth\RegisterController@verify_email');
